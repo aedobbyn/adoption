312da09b3a3b16ad0d3579d5b43c741908102c15
aedobbyn <adobbs11@gmail.com>
2018-03-14 20:11:09 -0500

    initial commit

---
+++ munge.R
@@ -0,0 +1,72 @@
+
+# Children in foster care waiting for adoption by race and Hispanic origin
+# from
+# http://datacenter.kidscount.org/data/tables/6674-children-in-foster-care-waiting-for-adoption-by-race-and-hispanic-origin#detailed/1/any/false/573,869,36,868,867/2638,2601,2600,2598,2603,2597,2602,1353/13723,13724
+
+# Data originally form Adoption and Foster Care Analysis and Reporting System (AFCARS), \n
+# made available through the National Data Archive on Child Abuse and Neglect
+
+library(here)
+library(tidyverse)
+library(stringr)
+
+adop_raw <- readxl::read_xlsx(here("data", "adop.xlsx")) 
+
+# --------------- Initial munge ----------------
+
+# Initial cleaning
+adop <- adop_raw %>% 
+  janitor::clean_names() %>% 
+  rename(
+    year = timeframe %>% as.numeric(),
+    count = data %>% as.numeric()
+  ) %>% 
+  filter(!dataformat == "Percent") %>%   # We can calculate percents ourselves
+  select(-dataformat) %>% 
+  arrange(year) %>% 
+  filter(!race_group == "Total")  # And sums
+
+
+# Relevel race
+adop$race_group <- factor(adop$race_group)
+
+new_race_labels <- levels(factor(adop$race_group)) %>% 
+  str_replace_all("Non-Hispanic ", "")
+new_race_labels[which(new_race_labels == "multiple race groups")] <- "Multiple"
+
+levels(adop$race_group) <- new_race_labels
+
+
+# --------------- Plot ----------------
+
+# Facet by every state
+ggplot(adop, 
+       aes(year, count, colour = race_group)) +
+  geom_line() +
+  facet_wrap(~ location)
+
+# Just Illinois
+ggplot(adop %>% filter(location == "Illinois"), 
+       aes(year, count, colour = race_group)) +
+  geom_line() +
+  theme_bw() +
+  ggtitle("Illinois") +
+  labs(x = "Year", y = "Count",
+       subtitle = "Children in Foster Care Waiting for Adoption",
+       colour = "Race",
+       caption = "Data from http://datacenter.kidscount.org/")
+
+# Whole US
+ggplot(adop %>% filter(location == "United States"), 
+       aes(year, count, colour = race_group)) +
+  geom_line() +
+  theme_bw() +
+  ggtitle("US-Wide") +
+  labs(x = "Year", y = "Count",
+       subtitle = "Children in Foster Care Waiting for Adoption",
+       colour = "Race",
+       caption = "Data from http://datacenter.kidscount.org/")
+
+
+
+
